ad8ab163fed23ba74da7d3fe2473360585d6304a
Johnny Wells <john.wells025@gmail.com>
2023-06-14 12:51:01 -0500

    Set and managed state, as wel as  configured two-way binding for each input field within the form to get the data that the user inputs into each input field in order to perform calculations on that data. Configured the default data for the form to display and when the user hits the reset button, the data is reset back to the initial/ default data configured for the form.

--- react-investment-calculator/src/components/UserInput/UserInput.js
+++ react-investment-calculator/src/components/UserInput/UserInput.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
+
+const initialUserInput = {
+    'current-savings': 10000,
+    'yearly-contribution': 1200,
+    'expected-return': 7,
+    'duration': 10
+};
 const UserInput = () => {
+
+ const [userInput, setUserInput] = useState(initialUserInput);
 
     const submitHandler = (event) => {
         event.preventDefault();
@@ -7,11 +16,17 @@ const UserInput = () => {
     };
 
     const resetHandler = () => {
-        console.log("Reset")
+        setUserInput(initialUserInput);
     }
 
     const inputChangeHandler = (input, value) => {
-        console.log(input, value);
+        setUserInput((prevInput) => {
+            return {
+                ...prevInput,
+                [input]: value
+            }
+        });
+
     };
 
     return (
@@ -19,11 +34,12 @@ const UserInput = () => {
             <div className="input-group">
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input onChange={(event)=>inputChangeHandler('current-savings', event.target.value)} type="number" id="current-savings" />
+                    <input onChange={(event)=>inputChangeHandler('current-savings', event.target.value)}
+                           value={userInput['current-savings']} type="number" id="current-savings" />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-                    <input onChange={(event)=>inputChangeHandler('yearly-contribution', event.target.value)}  type="number" id="yearly-contribution" />
+                    <input onChange={(event)=>inputChangeHandler('yearly-contribution', event.target.value)}  value={userInput['yearly-contribution']} type="number" id="yearly-contribution" />
                 </p>
             </div>
             <div className="input-group">
@@ -31,11 +47,11 @@ const UserInput = () => {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input onChange={(event)=>inputChangeHandler('expected-return', event.target.value)} type="number" id="expected-return" />
+                    <input onChange={(event)=>inputChangeHandler('expected-return', event.target.value)} value={userInput['expected-return']} type="number" id="expected-return" />
                 </p>
                 <p>
                     <label htmlFor="duration">Investment Duration (years)</label>
-                    <input onChange={(event)=>inputChangeHandler('duration', event.target.value)} type="number" id="duration" />
+                    <input onChange={(event)=>inputChangeHandler('duration', event.target.value)} value={userInput['duration']} type="number" id="duration" />
                 </p>
             </div>
             <p className="actions">
